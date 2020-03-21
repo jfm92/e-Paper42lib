3b4c54fd7b9a96ce61d6199dd6d827aa8be6a862
jfm92 <jfloresm92@gmail.com>
2020-03-21 20:30:02 +0100

    WIP config file to configure the display

---
+++ config.h
@@ -0,0 +1,24 @@
+/**
+ * Configuration file for the e-Paper lib for ESP32
+ **/
+
+/**
+ * Choose the SPI bus to use and select the pins
+ **/
+#define RST_PIN     26
+#define DC_PIN      27
+#define CS_PIN      15
+#define BUSY_PIN    25
+#define MOSI_PIN    14
+#define MISO_PIN    12 //Useless on this library but necessary to init the SPI
+#define SCK_PIN     13
+#define SPI_CLK  8000000 //1Mhz
+
+/**
+ * Screen configuration
+ * Default config for 4.2 inch display
+ **/
+
+#define SCR_WIDTH   400
+#define SCR_HEIGHT  300
+ 
